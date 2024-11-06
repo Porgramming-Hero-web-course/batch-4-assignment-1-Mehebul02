ef79d66dbca72f97f2b93ce7f70bcf7d6c385bc0
Mehebul02 <mehebul2123@gmail.com>
2024-11-06 23:57:52 +0600

    solve the problem_1

--- src/problem_1.ts
+++ src/problem_1.ts
@@ -1,1 +1,13 @@
-console.log('typescript')
+{
+
+    function sumArray(numbers: number[]): number {
+
+        return numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+
+
+    }
+
+    const array = [1, 2, 3, 4, 5]
+    console.log(sumArray(array));
+}
+
